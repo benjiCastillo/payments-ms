0b51b57f858b17d914f229a223fade6128c37ea9
BenjiCastillo <castilloeguezbenji@gmail.com>
2025-09-16 11:02:31 -0400

    implementacion de emit payment succeeded

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,12 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import Stripe from 'stripe';
 import { envs } from '../config/envs';
 import { PaymentSessionDto } from './dto/payment-session.dto';
 import { Request, Response } from 'express';
+import { Logger } from '@nestjs/common';
+import { NATS_SERVICE } from 'src/config/services';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class PaymentsService {
   private readonly stripe = new Stripe(envs.stripeSecret);
+  private readonly logger = new Logger('PaymentsService');
+
+  constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
 
   async createPaymentSession(paymentSessionDto: PaymentSessionDto) {
     const { currency, items, orderId } = paymentSessionDto;
@@ -36,7 +42,11 @@ export class PaymentsService {
       cancel_url: envs.stripeCancelUrl,
     });
 
-    return session;
+    return {
+      cancelUrl: session.cancel_url,
+      successUrl: session.success_url,
+      url: session.url,
+    };
   }
 
   stripeWebhook(req: Request, res: Response) {
@@ -52,20 +62,27 @@ export class PaymentsService {
         endpointSecret,
       );
     } catch (err) {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
       const message = err?.message as string;
       res.status(400).send(`Webhook Error: ${message}`);
       return;
     }
 
     let chargeSucceeded: Stripe.Charge;
+    let payload: {
+      stripePaymentId: string;
+      orderId: string;
+      receiptUrl: string;
+    };
     switch (event.type) {
       case 'charge.succeeded':
         chargeSucceeded = event.data.object;
-        console.log({
+        payload = {
+          stripePaymentId: chargeSucceeded.id,
           orderId: chargeSucceeded.metadata.order_id,
-        });
-
+          receiptUrl: chargeSucceeded.receipt_url as string,
+        };
+        this.logger.log({ payload });
+        this.client.emit('payment.succeeded', payload);
         break;
       default:
         console.log(`Unhandled event type: ${event.type}`);
